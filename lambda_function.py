51e709817ba418491fcb7826b273870035e4237b
Pavan Gandhi <pgandhi@zaizi.com>
2023-09-14 17:06:52 +0100

    update on formatting after 'black' installation

--- lambda_function.py
+++ lambda_function.py
@@ -13,12 +13,12 @@ import json
 def lambda_handler(event, context):
     # print(event)
     try:
-        if (event.get('Authorization', None)) is not None:
-            input_token = event['Authorization']
-        elif (event.get('authorizationToken', None)) is not None:
-            input_token = event['authorizationToken']
+        if (event.get("Authorization", None)) is not None:
+            input_token = event["Authorization"]
+        elif (event.get("authorizationToken", None)) is not None:
+            input_token = event["authorizationToken"]
         else:
-            return {'statusCode': 401, 'body': 'Unauthorized'}
+            return {"statusCode": 401, "body": "Unauthorized"}
 
         if input_token is not None:
             token = input_token
@@ -26,41 +26,35 @@ def lambda_handler(event, context):
             # token = event['Authorization'] or event['authorizationToken']
             token_response = json.loads(token_validator.validate_access_token(token))
 
-            if token_response['statusCode'] == 200:
-                if token_response['isActive']:
-                    return generate_policy('user', 'Allow', event['methodArn'])
+            if token_response["statusCode"] == 200:
+                if token_response["isActive"]:
+                    return generate_policy("user", "Allow", event["methodArn"])
                 else:
-                    return generate_policy('user', 'Deny', event['methodArn'])
+                    return generate_policy("user", "Deny", event["methodArn"])
             else:
-                return generate_policy('user', 'Deny', event['methodArn'])
+                return generate_policy("user", "Deny", event["methodArn"])
         else:
-            return {'statusCode': 401, 'body': 'Unauthorized'}
+            return {"statusCode": 401, "body": "Unauthorized"}
     except:
-        return {'statusCode': 401, 'body': 'Unauthorized'}
+        return {"statusCode": 401, "body": "Unauthorized"}
 
 
 def generate_policy(principal_id, effect, resource):
-    auth_response = {
-        'principalId': principal_id
-    }
+    auth_response = {"principalId": principal_id}
 
     if effect and resource:
         policy_document = {
-            'Version': '2012-10-17',
-            'Statement': [
-                {
-                    'Action': 'execute-api:Invoke',
-                    'Effect': effect,
-                    'Resource': resource
-                }
-            ]
+            "Version": "2012-10-17",
+            "Statement": [
+                {"Action": "execute-api:Invoke", "Effect": effect, "Resource": resource}
+            ],
         }
-        auth_response['policyDocument'] = policy_document
+        auth_response["policyDocument"] = policy_document
 
-    auth_response['context'] = {
-        'stringKey': 'stringval',
-        'numberKey': 123,
-        'booleanKey': True
+    auth_response["context"] = {
+        "stringKey": "stringval",
+        "numberKey": 123,
+        "booleanKey": True,
     }
 
     return auth_response
--- token_validator.py
+++ token_validator.py
@@ -10,40 +10,50 @@ def validate_access_token(token):
     KEYCLOAK_BASE_URI = parameter.get_parameter_store_key_value("KEYCLOAK_BASE_URI")
     KEYCLOAK_CLIENT_ID = parameter.get_parameter_store_key_value("KEYCLOAK_CLIENT_ID")
     KEYCLOAK_REALM_NAME = parameter.get_parameter_store_key_value("KEYCLOAK_REALM_NAME")
-    KEYCLOAK_CLIENT_SECRET = parameter.get_parameter_store_key_value("KEYCLOAK_CLIENT_SECRET")
+    KEYCLOAK_CLIENT_SECRET = parameter.get_parameter_store_key_value(
+        "KEYCLOAK_CLIENT_SECRET"
+    )
 
-    KEYCLOAK_INTROSPECTION_ENDPOINT = (KEYCLOAK_BASE_URI + '/realms/' + KEYCLOAK_REALM_NAME
-                                       + '/protocol/openid-connect/token/introspect')
+    KEYCLOAK_INTROSPECTION_ENDPOINT = (
+        KEYCLOAK_BASE_URI
+        + "/realms/"
+        + KEYCLOAK_REALM_NAME
+        + "/protocol/openid-connect/token/introspect"
+    )
 
     http = urllib3.PoolManager(ca_certs=certifi.where())
-    data = {"client_id": KEYCLOAK_CLIENT_ID, "client_secret": KEYCLOAK_CLIENT_SECRET, "token": token}
+    data = {
+        "client_id": KEYCLOAK_CLIENT_ID,
+        "client_secret": KEYCLOAK_CLIENT_SECRET,
+        "token": token,
+    }
     encoded_data = urllib.parse.urlencode(data)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
     # bearerToken = "Bearer " + token
     # headers = {'Content-Type': 'application/x-www-form-urlencoded', 'Authorization': bearerToken}
 
     try:
-        request = http.request('POST', KEYCLOAK_INTROSPECTION_ENDPOINT, headers=headers, body=encoded_data)
-        response_data = json.loads(request.data.decode('utf-8'))
-        if response_data['active']:
+        request = http.request(
+            "POST", KEYCLOAK_INTROSPECTION_ENDPOINT, headers=headers, body=encoded_data
+        )
+        response_data = json.loads(request.data.decode("utf-8"))
+        if response_data["active"]:
             response = {
-                'statusCode': 200,
-                'isActive': response_data['active'],
-                'error': ''
+                "statusCode": 200,
+                "isActive": response_data["active"],
+                "error": "",
             }
         else:
-            response = {
-                'statusCode': 403,
-                'isActive': False,
-                'error': ''
-            }
+            response = {"statusCode": 403, "isActive": False, "error": ""}
         logging.info("Access token has been validated successfully")
     except Exception as e:
-        logging.error("Failed to validate access token for user with error : %s", str(e))
+        logging.error(
+            "Failed to validate access token for user with error : %s", str(e)
+        )
         response = {
-            'statusCode': 403,
-            'isActive': False,
-            'error': 'Failed to validate access token for user with error : ' + str(e)
+            "statusCode": 403,
+            "isActive": False,
+            "error": "Failed to validate access token for user with error : " + str(e),
         }
 
     return json.dumps(response)
